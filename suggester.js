63db93404222d64a7a5b516f4dea3f0b96c99edf
Mubeezi <victor.muwanga@gmail.com>
2019-03-27 12:09:12 +0300

    prints selected value in DOM for age

--- suggester.js
+++ suggester.js
@@ -1,47 +1,38 @@
-$(function()
-  {
-    $('#form').validate(
-      {
-      rules:
-        {
-          age:{ required:true },
-          other:
-          {
-            required:
-            {
-            depends:function()
-            {
-              var sel = $('input[name=age]:checked', '#form').val();
+$(document).ready(function(event) {
+    $("#vacform").on('submit',function(event) {
+      event.preventDefault();
 
-              if(sel =='other')
-                {
-                return true;
-                }
-              else
-                {
-                return false;
-                }
-            }
-            }
-          }
-        },
-        messages:
-        {
-          channel:
-          {
-            required:"Please select an option"
-          }
-        },
-        errorPlacement: function(error, element) {
-            if ( element.is(":radio") )
-            {
-                error.appendTo( element.parents('.container') );
-            }
-            else
-            { // This is the default behavior
-                error.insertAfter( element );
-            }
-         }
-      });
+        var ageID = $("input[name='age']:checked").val();
+        var weatherID = $("input[name='scenery']:checked").val();
+        var sceneryID= $("input[name='activity']:checked").val();
+        var expenditureID =$("input[name='expenditure']:checked").val();
+        var activityID = $("input[name='weather']:
+        checked").val();
 
-  });
+
+        if (ageID="1-10"  )
+      }
+    )});
+
+
+
+
+
+
+/*  function validate(){
+var valid=false;
+var ageID=document.vacform.age;
+for(var i=o;i<ageID.length;i++){
+  if (ageID[i].checked){
+    valid=true;
+    break;
+  }
+}
+if(valid){
+  alert("validation");
+}
+else{
+  alert("please select a mode of payment");
+  return false;
+}
+};*/
