a5f961a319a294383c4f2077d0df9f69f4b4e0f6
Mubeezi <victor.muwanga@gmail.com>
2019-03-27 12:46:58 +0300

    working redirection

--- suggester.js
+++ suggester.js
@@ -3,14 +3,17 @@ $(document).ready(function(event) {
       event.preventDefault();
 
         var ageID = $("input[name='age']:checked").val();
-        var weatherID = $("input[name='scenery']:checked").val();
-        var sceneryID= $("input[name='activity']:checked").val();
+        var weatherID = $("input[name='weather']:checked").val();
+        var sceneryID= $("input[name='scenery']:checked").val();
         var expenditureID =$("input[name='expenditure']:checked").val();
-        var activityID = $("input[name='weather']:checked").val();
+        var activityID = $("input[name='activity']:checked").val();
 
 
+        // Check if the condition is true and then redirect.
+  if ( ageID==="1-10"||activityID==="Exciting" ) {
+   window.location.href = "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/43a69f1b544e4844b4cbfd529ce7d352";
+  }
 
-        console.log(ageID);
       }
     )});
 
