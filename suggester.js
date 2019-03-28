26a3d3f86a77c18aaa7c40701a2d6aff9911c267
Mubeezi <victor.muwanga@gmail.com>
2019-03-28 21:50:09 +0300

    suggests two vacation plces

--- suggester.js
+++ suggester.js
@@ -10,8 +10,13 @@ $(document).ready(function(event) {
 
 
         // Check if the condition is true and then redirect.
-  if ( ageID==="1-10"||activityID==="Exciting" ) {
-   window.location.href = "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/43a69f1b544e4844b4cbfd529ce7d352";
+  if ( ageID==="1-10" ||ageID==="10-20"||activityID==="Exciting" ||activityID==="Very Exciting"|| expenditureID==="100-500" ||expenditureID==="500-1000" ||weatherID==="SUNNY"||weatherID==="CHILLED"||sceneryID==="ROCKY"||sceneryID==="AQUATIC" ) {
+   window.location.href = "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/43a69f1b544e4844b4cbfd529ce7d352";}
+
+   if ( ageID==="20-40" ||ageID==="40+"||activityID==="Minimum" ||activityID==="Moderate"|| expenditureID==="1000-1500" ||expenditureID==="1500+" ||weatherID==="PRECIPITATION"||weatherID==="COOLED"||sceneryID==="LOW-LYING"||sceneryID==="VAST-SHAPED" ) {
+    window.location.href = "https://www.responsibletravel.com/ImagesClient/indian-himalayas-topbox-map.gif";
+
+
   }
 
       }
